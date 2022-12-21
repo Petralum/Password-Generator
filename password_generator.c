53c03d394018e2f0f239675a7dfa441c1354df1f
tobikiller <edwardnguimeya@yahoo.com>
2022-12-21 13:25:42 +0100

    user authenification done

--- password_generator.c
+++ password_generator.c
@@ -4,24 +4,56 @@
 #include <string.h>
 #define MAX_LIMIT 20
 
+int password(int password_length)
+{
+
+	printf("\t");
+
+	if (password_length >= 8)
+	{
+		for (int i = 0; i < password_length; i++)
+		{
+			printf("*");
+		}
+	}
+	else
+	{
+		printf("password lenght does not reach the minimal number of lenght");
+		exit(EXIT_FAILURE);
+		return (1);
+	}
+
+	
+
+}
+
 int main(void)
 {
 	char nameuser[MAX_LIMIT];
+	int password_length;
 	printf("Enter your name please\n");
-	// fgets(nameuser, MAX_LIMIT, stdin);
+	//user authentification
 	scanf("%s", nameuser);
 	if (strcmp(nameuser, "Edward")==0 || strcmp(nameuser, "Petra")==0)
 	{
 	printf("\n\t********************\n\n");
 	printf("Welcome %s to the password Generator\n\n", nameuser);
 	printf("\t********************\n");
+
+	printf("\n");
+	printf("Please Enter the lenght of password desire\n");
+	scanf("%d", &password_length);
+
+	password(password_length);
+	return(0);
+
 	}
 	else
 	{
 		printf("Wrong user your are not allowed to execute this programe ask the admin for permission");
 		exit(EXIT_FAILURE);
+		return (1);
 	}
 	
-
-	return(0);
+	
 }
